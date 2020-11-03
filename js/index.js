885f44c3b47a07ace9d1942107b8cdd5a51286a7
ThreeMint <mistsripples@163.com>
2020-11-03 22:04:23 +0800

    add number effect

--- js/index.js
+++ js/index.js
@@ -64,7 +64,6 @@ function handlePrevClicked() {
 }
 
 // back to top
-//动画函数
 //obj,目标对象  target，目标位置    
 function animate(obj, target, callback) {
   //解决：先清除以前的定时器，只保留当前的一个定时器执行
@@ -85,7 +84,6 @@ function animate(obj, target, callback) {
     window.scroll(0, window.pageYOffset + step);
   }, 15);
 }
-
 const backTop = document.querySelector('.back-content');
 backTop.addEventListener('click', function () {
   animate(window, 0);
@@ -93,21 +91,86 @@ backTop.addEventListener('click', function () {
 
 const fullbar = document.querySelector('.fullbar');
 const fullbarItem = document.querySelector('.fullbar-item');
+const review = document.querySelector('.reviews');
+const reviewCount = document.querySelector('#comment-count .title-number');
 let cHeight = document.documentElement.clientHeight;
+let finished = true;//scroll事件中，执行一次标记
 document.addEventListener('scroll', (e) => {
   let sTop = document.documentElement.scrollTop;
-  // console.log(sTop, cHeight);
 
-  let fullbarsoffsetTop = fullbar.offsetTop;
-  // let fullbarsoffsetHeight = fullbarsoffsetTop + 177;
-  // let fullbarsHeight = fullbar.scrollHeight;
-  // console.log(fullbar.scrollHeight);
-  // console.log(fullbarsoffsetTop,fullbarsoffsetHeight, fullbarsHeight);
-
-  // console.log("窗口的75%," + cHeight*0.75);
-  // console.log("bar上方 - 窗口一半," + fheight);
-  let fheight = fullbarsoffsetTop - (cHeight*0.75);
+  let fheight = fullbar.offsetTop - (cHeight * 0.75);
   if (sTop >= fheight) {
     fullbarItem.classList.add("fullbar-show");
   }
+
+  let reviewHeight = review.offsetTop - (cHeight * 0.5);
+  if (finished && sTop >= reviewHeight) {
+    finished = false;
+    DynamicNumber.show("co-count", 3016);
+  }
 })
+
+
+/**
+ * 在指定的 DOM 元素中动态显示数值
+ * 作者：triplestudio@sina.com
+ *
+ * @param elementId :  DOM 元素ID
+ * @param number  :  数值
+ */
+var DynamicNumber = {};
+DynamicNumber.NumberList = {};
+DynamicNumber.show = function (elementId, number) {
+  // 1. 已建立过对象直接调用
+  var dynaNum = this.NumberList[elementId];
+  if (dynaNum) {
+   dynaNum.step = 0;
+   dynaNum.desNumber = number;
+   dynaNum.render();
+   return;
+  }
+  // 2. 创建动态数字绘制对象
+  dynaNum = new function (_elementId) {
+   this.elementId = _elementId;
+   this.preNumber = 0; // 变化过程值
+   this.desNumber = 0; // 目标数值，最终显示值
+   this.step = 0;  // 变化步长，支持正向反向
+   // 绘制过程
+   this.render = function () {
+    // （1）结束条件
+    if (this.preNumber == this.desNumber) {
+     this.step = 0;
+     return;
+    }
+    // （2）步长设置（计划 2 秒完成 40*50=2000）
+    if (this.step == 0) {
+     this.step = Math.round((this.desNumber - this.preNumber) / 40);
+     if (this.step == 0) this.step = (this.desNumber - this.preNumber > 0) ? 1 : -1;
+    }
+    // （3）走一步
+    this.preNumber += this.step;
+    if (this.step < 0) {
+     if (this.preNumber < this.desNumber) this.preNumber = this.desNumber;
+    } else {
+     if (this.preNumber > this.desNumber) this.preNumber = this.desNumber;
+    }
+    // （4）显示
+    document.getElementById(this.elementId).innerHTML = this.preNumber;
+    // （5）每秒绘制 20 次（非精确值）
+     var _this = this;
+     setTimeout(function () {
+       _this.render();
+     }, 50);
+   };
+  } (elementId);
+  // 3. 登记绑定元素ID 
+  DynamicNumber.NumberList[elementId] = dynaNum;
+  // 4. 调用绘制
+  dynaNum.step = 0;
+  dynaNum.desNumber = number;
+  dynaNum.render();
+};
+
+// setInterval(function () { 
+//   DynamicNumber.show("co-count", DynamicNumber.NumberList["co-count"].desNumber + 300);
+// }, 5000);
